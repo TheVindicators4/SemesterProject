685d7b7fc0aee046e0cba8d4052ebae1e5c89c82
ogparry1 <ogparry1@ufl.edu>
2017-10-26 17:50:05 -0400

    switching branches

--- modules/users/client/config/users.client.routes.js
+++ modules/users/client/config/users.client.routes.js
@@ -3,8 +3,8 @@
 
   // Setting up route
   angular
-    .module('users.routes')
-    .config(routeConfig);
+  .module('users.routes')
+  .config(routeConfig);
 
   routeConfig.$inject = ['$stateProvider'];
 
@@ -12,71 +12,86 @@
     // Users state routing
     $stateProvider
     .state('authentication', {
-        abstract: true,
-        url: '/authentication',
-        templateUrl: '/modules/users/client/views/authentication/authentication.client.view.html',
-        controller: 'AuthenticationController',
-        controllerAs: 'vm'
-      })
-      .state('authentication.signup', {
-        url: '/signup',
-        templateUrl: '/modules/users/client/views/authentication/signup.client.view.html',
-        controller: 'AuthenticationController',
-        controllerAs: 'vm',
-        data: {
-          pageTitle: 'Signup'
-        }
-      })
-      .state('authentication.signin', {
-        url: '/signin?err',
-        templateUrl: '/modules/users/client/views/authentication/signin.client.view.html',
-        controller: 'AuthenticationController',
-        controllerAs: 'vm',
-        data: {
-          pageTitle: 'Signin'
-        }
-      })
-      .state('password', {
-        abstract: true,
-        url: '/password',
-        template: '<ui-view/>'
-      })
-      .state('password.forgot', {
-        url: '/forgot',
-        templateUrl: '/modules/users/client/views/password/forgot-password.client.view.html',
-        controller: 'PasswordController',
-        controllerAs: 'vm',
-        data: {
-          pageTitle: 'Password forgot'
-        }
-      })
-      .state('password.reset', {
-        abstract: true,
-        url: '/reset',
-        template: '<ui-view/>'
-      })
-      .state('password.reset.invalid', {
-        url: '/invalid',
-        templateUrl: '/modules/users/client/views/password/reset-password-invalid.client.view.html',
-        data: {
-          pageTitle: 'Password reset invalid'
-        }
-      })
-      .state('password.reset.success', {
-        url: '/success',
-        templateUrl: '/modules/users/client/views/password/reset-password-success.client.view.html',
-        data: {
-          pageTitle: 'Password reset success'
-        }
-      })
-      .state('password.reset.form', {
-        url: '/:token',
-        templateUrl: '/modules/users/client/views/password/reset-password.client.view.html',
-        controller: 'PasswordController',
-        controllerAs: 'vm',
-        data: {
-          pageTitle: 'Password reset form'
-        }
-      });
-  }
+      abstract: true,
+      url: '/authentication',
+      templateUrl: '/modules/users/client/views/authentication/authentication.client.view.html',
+      controller: 'AuthenticationController',
+      controllerAs: 'vm'
+    })
+    .state('authentication.signup', {
+      url: '/signup',
+      templateUrl: '/modules/users/client/views/authentication/signup.client.view.html',
+      controller: 'AuthenticationController',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Signup'
+      }
+    })
+    .state('authentication.signin', {
+      url: '/signin?err',
+      templateUrl: '/modules/users/client/views/authentication/signin.client.view.html',
+      controller: 'AuthenticationController',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Signin'
+      }
+    })
+    .state('password', {
+      abstract: true,
+      url: '/password',
+      template: '<ui-view/>'
+    })
+    .state('password.forgot', {
+      url: '/forgot',
+      templateUrl: '/modules/users/client/views/password/forgot-password.client.view.html',
+      controller: 'PasswordController',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Password forgot'
+      }
+    })
+    .state('password.reset', {
+      abstract: true,
+      url: '/reset',
+      template: '<ui-view/>'
+    })
+    .state('password.reset.invalid', {
+      url: '/invalid',
+      templateUrl: '/modules/users/client/views/password/reset-password-invalid.client.view.html',
+      data: {
+        pageTitle: 'Password reset invalid'
+      }
+    })
+    .state('password.reset.success', {
+      url: '/success',
+      templateUrl: '/modules/users/client/views/password/reset-password-success.client.view.html',
+      data: {
+        pageTitle: 'Password reset success'
+      }
+    })
+    .state('password.reset.form', {
+      url: '/:token',
+      templateUrl: '/modules/users/client/views/password/reset-password.client.view.html',
+      controller: 'PasswordController',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Password reset form'
+      }
+    })
+    .state('settings', {
+      abstract: true,
+      url: '/',
+      template: '<ui-view/>'
+    })
+    .state('settings.admin', {
+      url: '/admin/settings',
+      templateUrl: '/modules/users/client/views/settings/settings.client.view.html',
+      controller: 'SettingsController',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Admin Settings'
+      }
+    })
+  });
+}
 }());
