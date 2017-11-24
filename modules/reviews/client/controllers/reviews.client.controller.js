fd0d6ebfa89b7ca269530cef245313e107e0bc1e
Beverly Roberts <beverlyr380@ufl.edu>
2017-11-24 12:54:59 -0500

    New Reviews Module, adjusted Blog routes and fixed inject error

--- modules/reviews/client/controllers/reviews.client.controller.js
+++ modules/reviews/client/controllers/reviews.client.controller.js
@@ -12,6 +12,33 @@
 
     vm.review = review;
     vm.authentication = Authentication;
+  //  vm.form = {};
+    //vm.save = save;
+
+    // Save Review
+  /*  function save(isValid) {
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.form.reviewForm');
+        return false;
+      }
+
+      // Create a new review, or update the current instance
+      vm.review.createOrUpdate()
+        .then(successCallback)
+        .catch(errorCallback);
+
+      function successCallback(res) {
+        $state.go('admin.reviews.list'); // should we send the User to the list or the updated Review's view?
+        Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Review saved successfully!' });
+      }
+
+      function errorCallback(res) {
+        Notification.error({ message: res.data.message, title: '<i class="glyphicon glyphicon-remove"></i> Review save error!' });
+      }
+    }*/
+
+
+
 
   }
 }());
