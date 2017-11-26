04ba55153dda71954c1f40a463cd0d74b4b741af
Chad Lim <chadzlim@yahoo.com>
2017-11-26 15:40:33 -0500

    Adding approval/deny functionality

--- modules/reviews/server/controllers/reviews.server.controller.js
+++ modules/reviews/server/controllers/reviews.server.controller.js
@@ -1,118 +1,118 @@
 'use strict';
 
 /**
- * Module dependencies
- */
+* Module dependencies
+*/
 var path = require('path'),
-  mongoose = require('mongoose'),
-  Review = mongoose.model('Review'),
-  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
+mongoose = require('mongoose'),
+Review = mongoose.model('Review'),
+errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
 
 /**
- * Create an review
- */
+* Create an review
+*/
 exports.create = function (req, res) {
-  var review = new Review(req.body);
-  review.user = req.body.user;
-console.log(review.user);
-console.log(req.body.user);
-  review.save(function (err) {
-    if (err) {
-      return res.status(422).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.json(review);
-    }
-  });
+	var review = new Review(req.body);
+	review.user = req.body.user;
+	console.log(review.user);
+	console.log(req.body.user);
+	review.save(function (err) {
+		if (err) {
+			return res.status(422).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(review);
+		}
+	});
 };
 
 /**
- * Show the current review
- */
+* Show the current review
+*/
 exports.read = function (req, res) {
-  // convert mongoose document to JSON
-  var review = req.review ? req.review.toJSON() : {};
+	// convert mongoose document to JSON
+	var review = req.review ? req.review.toJSON() : {};
 
-  // Add a custom field to the Review, for determining if the current User is the "owner".
-  // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Review model.
-  review.isCurrentUserOwner = !!(req.user && review.user && review.user._id.toString() === req.user._id.toString());
+	// Add a custom field to the Review, for determining if the current User is the "owner".
+	// NOTE: This field is NOT persisted to the database, since it doesn't exist in the Review model.
+	// review.isCurrentUserOwner = !!(req.user && review.user && review.user._id.toString() === req.user._id.toString());
 
-  res.json(review);
+	res.json(review);
 };
 
 /**
- * Update an review
- */
+* Update an review
+*/
 exports.update = function (req, res) {
-  var review = req.review;
+	var review = req.review;
 
-  review.title = req.body.title;
-  review.content = req.body.content;
-
-  review.save(function (err) {
-    if (err) {
-      return res.status(422).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.json(review);
-    }
-  });
+	review.title = req.body.title;
+	review.content = req.body.content;
+	review.approve = true;
+	review.save(function (err) {
+		if (err) {
+			return res.status(422).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(review);
+		}
+	});
 };
 
 /**
- * Delete an review
- */
+* Delete an review
+*/
 exports.delete = function (req, res) {
-  var review = req.review;
+	var review = req.review;
 
-  review.remove(function (err) {
-    if (err) {
-      return res.status(422).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.json(review);
-    }
-  });
+	review.remove(function (err) {
+		if (err) {
+			return res.status(422).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(review);
+		}
+	});
 };
 
 /**
- * List of Reviews
- */
+* List of Reviews
+*/
 exports.list = function (req, res) {
-  Review.find().sort('-created').exec(function (err, reviews) {
-    if (err) {
-      return res.status(422).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.json(reviews);
-    }
-  });
+	Review.find().sort('-created').exec(function (err, reviews) {
+		if (err) {
+			return res.status(422).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(reviews);
+		}
+	});
 };
 
 /**
- * Review middleware
- */
+* Review middleware
+*/
 exports.reviewByID = function (req, res, next, id) {
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({
-      message: 'Review is invalid'
-    });
-  }
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(400).send({
+			message: 'Review is invalid'
+		});
+	}
 
-  Review.findById(id).populate('user', 'displayName').exec(function (err, review) {
-    if (err) {
-      return next(err);
-    } else if (!review) {
-      return res.status(404).send({
-        message: 'No review with that identifier has been found'
-      });
-    }
-    req.review = review;
-    next();
-  });
+	Review.findById(id).exec(function (err, review) {
+		if (err) {
+			return next(err);
+		} else if (!review) {
+			return res.status(404).send({
+				message: 'No review with that identifier has been found'
+			});
+		}
+		req.review = review;
+		next();
+	});
 };
