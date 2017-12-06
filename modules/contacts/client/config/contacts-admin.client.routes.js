fee2dff535ffda61381c53c669007508c76c2eaf
ogparry1 <ogparry1@ufl.edu>
2017-12-06 11:04:04 -0500

    finished product

--- modules/contacts/client/config/contacts-admin.client.routes.js
+++ modules/contacts/client/config/contacts-admin.client.routes.js
@@ -1,53 +1,62 @@
-﻿(function () {
+(function () {
   'use strict';
 
   angular
-    .module('contacts.admin.routes')
-    .config(routeConfig);
+  .module('contacts.admin.routes')
+  .config(routeConfig);
 
   routeConfig.$inject = ['$stateProvider'];
 
   function routeConfig($stateProvider) {
     $stateProvider
-      .state('admin.contacts', {
-        abstract: true,
-        url: '/contacts',
-        template: '<ui-view/>'
-      })
-      .state('admin.contacts.list', {
-        url: '',
-        templateUrl: '/modules/contacts/client/views/admin/list-contacts.client.view.html',
-        controller: 'ContactsAdminListController',
-        controllerAs: 'vm',
-        data: {
-          roles: ['admin']
-        }
-      })
-      .state('admin.contacts.create', {
-        url: '/create',
-        templateUrl: '/modules/contacts/client/views/admin/form-contact.client.view.html',
-        controller: 'ContactsAdminController',
-        controllerAs: 'vm',
-        data: {
-          roles: ['admin']
-        },
-        resolve: {
-          contactResolve: newContact
-        }
-      })
-      .state('admin.contacts.edit', {
-        url: '/:contactId/edit',
-        templateUrl: '/modules/contacts/client/views/admin/form-contact.client.view.html',
-        controller: 'ContactsAdminController',
-        controllerAs: 'vm',
-        data: {
-          roles: ['admin'],
-          pageTitle: '{{ contactResolve.title }}'
-        },
-        resolve: {
-          contactResolve: getContact
-        }
-      });
+    .state('admin.contacts', {
+      abstract: true,
+      url: '/contacts',
+      template: '<ui-view/>'
+    })
+    .state('admin.contacts.list', {
+      url: '',
+      templateUrl: '/modules/contacts/client/views/admin/list-contacts.client.view.html',
+      controller: 'ContactsAdminListController',
+      controllerAs: 'vm',
+      data: {
+        roles: ['admin']
+      }
+    })
+    .state('admin.contacts.view', {
+      url: '',
+      templateUrl: '/modules/contacts/client/views/admin/view-contact.client.view.html',
+      controller: 'ContactsAdminListController',
+      controllerAs: 'vm',
+      data: {
+        roles: ['admin']
+      }
+    })
+    .state('admin.contacts.create', {
+      url: '/create',
+      templateUrl: '/modules/contacts/client/views/admin/form-contact.client.view.html',
+      controller: 'ContactsAdminController',
+      controllerAs: 'vm',
+      data: {
+        roles: ['admin']
+      },
+      resolve: {
+        contactResolve: newContact
+      }
+    })
+    .state('admin.contacts.edit', {
+      url: '/:contactId/edit',
+      templateUrl: '/modules/contacts/client/views/admin/form-contact.client.view.html',
+      controller: 'ContactsAdminController',
+      controllerAs: 'vm',
+      data: {
+        roles: ['admin'],
+        pageTitle: '{{ contactResolve.title }}'
+      },
+      resolve: {
+        contactResolve: getContact
+      }
+    });
   }
 
   getContact.$inject = ['$stateParams', 'ContactsService'];
